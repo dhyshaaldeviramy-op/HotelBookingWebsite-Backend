6c2d90cf3cd26eeda0b797a33921168e902a24a2
aravindhan14v <aravindhan142005@gmail.com>
2026-04-27 12:16:13 +0530

    Data and DTO created

---
+++ HotelBookingWebsite-HCL/Data/AppDbContext.cs
@@ -0,0 +1,19 @@
+﻿using HotelBooking.Model;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace HotelBooking.Data
+{
+    public class AppDbContext:DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options):base(options)
+        {
+        }
+        public DbSet<Booking> Bookings { get; set; }
+        public DbSet<Hotel> Hotels { get; set; }
+        public DbSet<Payment> Payments { get; set; }
+        public DbSet<Promotion> Promotions { get; set; }
+        public DbSet<Room> Rooms { get; set; }
+        public DbSet<User> Users { get; set; }
+    }
+}
