c23672843b479f2feaaf4b26ea4d5b82b9fffbac
aravindhan14v <aravindhan142005@gmail.com>
2026-04-27 12:30:25 +0530

    Controllers Created

--- HotelBookingWebsite-HCL/Model/User.cs
+++ HotelBookingWebsite-HCL/Model/User.cs
@@ -9,7 +9,7 @@ namespace HotelBooking.Model
         public string email { get; set; }
         public string password { get; set; }
         public string Role { get; set; }
-        //public ICollection<Booking> Bookings { get; set; }
+        
 
 
     }
