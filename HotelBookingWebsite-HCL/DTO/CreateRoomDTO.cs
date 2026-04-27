be28cd99c60af001ff20564085b430b9690212aa
Dhyshaal Devira <dhyshaaldevira@gmail.com>
2026-04-27 12:40:52 +0530

    models created

---
+++ HotelBookingWebsite-HCL/DTO/CreateRoomDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace HotelBooking.DTO
+{
+    public class CreateRoomDTO
+    {
+        public int HotelId { get; set; }
+        public string Category { get; set; }
+        public int Price { get; set; }
+        public int Availability { get; set; }
+    }
+}
--- HotelBookingWebsite-HCL/Program.cs
+++ HotelBookingWebsite-HCL/Program.cs
@@ -1,7 +1,12 @@
+using HotelBooking.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(
+        builder.Configuration.GetConnectionString("DefaultConnection")));
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
